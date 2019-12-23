6d2a6e70620b5fc2e56d5c3b236eff4bbdba53f8
Gasper <gasper@gmx.com>
2019-12-23 08:00:54 +0100

    Working part 1 of day 18

--- day18/eighteen.rs
+++ day18/eighteen.rs
@@ -1,21 +1,24 @@
 extern crate pathfinding;
+extern crate itertools;
 
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::collections::{HashMap, VecDeque, HashSet};
-use pathfinding::prelude::dijkstra;
+use pathfinding::prelude::bfs;
+use itertools::Itertools;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 struct PossiblePath {
     keys: Vec<char>,
     current_position: (usize, usize),
+    current_key: char,
     remaining: HashMap<char, (usize, usize)>,
     path_length: usize,
 }
 
 fn main() {
-    let input_file = match File::open("input3.txt") {
+    let input_file = match File::open("input.txt") {
         Err(_) => panic!("Could not open input.txt"),
         Ok(file) => file,
     };
@@ -25,144 +28,210 @@ fn main() {
         .map(|line| line.unwrap().chars().collect())
         .collect();
     
-    let key_positions = find_keys(&map);
+    let mut key_positions = find_keys(&map);
     println!("Key pos: {:?}", key_positions);
     
     let mut starting = PossiblePath {
-        keys: Vec::new(),
+        keys: vec!['@'],
         current_position: (0, 0),
-        remaining: key_positions,
+        current_key: '@',
+        remaining: key_positions.clone(),
         path_length: 0,
     };
     starting.current_position = location_of(&map, '@').unwrap();
-    let starting_options = starting.possible_continuations(&map);
+    &key_positions.insert('@', starting.current_position);
+
+    let key_graph = build_key_graph(&map, key_positions);
 
     let mut checked_paths: HashMap<String, usize> = HashMap::new();
     let mut continuations: VecDeque<PossiblePath> = VecDeque::new();
+    let starting_options = starting.possible_continuations(&key_graph);
     continuations.extend(starting_options);
+
     let mut shortest = 6070;
+    let mut shortest_path = starting;
     while let Some(next_path) = continuations.pop_front() {
-        if let Some(len) = checked_paths.get(&unique_path_pattern(&next_path.keys)) {
-            if *len < next_path.path_length {
+        if let Some(len) = checked_paths.get(&unique_path_pattern(&next_path.keys, next_path.current_key)) {
+            if *len <= next_path.path_length {
                 continue;
             }
         }
-        checked_paths.insert(unique_path_pattern(&next_path.keys), next_path.path_length);
+        checked_paths.insert(unique_path_pattern(&next_path.keys, next_path.current_key), next_path.path_length);
 
-        let mut next_continuations = next_path.possible_continuations(&map);
+        let next_continuations = next_path.possible_continuations(&key_graph);
 
         if next_continuations.is_empty() {
             if next_path.has_all_keys() && next_path.path_length < shortest {
+                println!("New shortest: {}", next_path.path_length);
                 shortest = next_path.path_length;
+                shortest_path = next_path;
             }
         }
         else {
-            println!("more cont: {}", continuations.len());
-            continuations.extend(next_continuations.into_iter().filter(|c| c.path_length < shortest));
+            for candidate in next_continuations.into_iter() {
+                continuations.push_back(candidate);
+            }
         }
     }
 
     println!("Shortest path: {}", shortest);   
-    // 6616 
+    println!("Keys: {:?}", shortest_path);
 }
 
-fn unique_path_pattern(keys: &Vec<char>) -> String {
-    let mut start: Vec<char> = keys[0..(keys.len() - 1)].to_vec();
+
+fn build_key_graph(map: &Vec<Vec<char>>, key_positions: HashMap<char, (usize, usize)>)
+-> HashMap<char, HashMap<char, (usize, HashSet<char>, HashSet<char>)>> {
+    
+    let doors_on_map = find_doors(map);
+
+    let mut shortest_paths = HashMap::new();
+    for (key, key_position) in &key_positions {
+
+        let mut key_connections = HashMap::new();
+
+        for (key_target, key_position_target) in &key_positions {
+            if *key == *key_target {
+                continue;
+            }
+
+            let target_path = bfs(key_position, 
+                |p| successors(map, *p),
+                |p| *p == *key_position_target).unwrap();
+            
+            let key_doors = doors_on_path(&target_path, &doors_on_map);
+            let key_gratis_keys = keys_on_path(&target_path, &key_positions);
+
+            key_connections.insert(*key_target, (target_path.len() - 1, key_doors, key_gratis_keys));
+        }
+
+        shortest_paths.insert(*key, key_connections);
+    }
+
+    return shortest_paths;
+}
+
+fn doors_on_path(path: &Vec<(usize, usize)>, door_positions: &HashMap<char, (usize, usize)>) -> HashSet<char> {
+    let mut keys = HashSet::new();
+    for position in path.into_iter() {
+        for (key, pos) in door_positions {
+            if *pos == *position {
+                keys.insert(*key);
+            }
+        }
+    }
+    return keys;
+}
+
+fn keys_on_path(path: &Vec<(usize, usize)>, key_positions: &HashMap<char, (usize, usize)>) -> HashSet<char> {
+    let mut keys = HashSet::new();
+    for position in path.into_iter().skip(1) {
+        for (key, pos) in key_positions {
+            if *pos == *position {
+                keys.insert(*key);
+            }
+        }
+    }
+    return keys;
+}
+
+
+fn successors(map: &Vec<Vec<char>>, position: (usize, usize)) -> Vec<(usize, usize)> {
+        
+    fn reachable(tile: char) -> bool {
+        return tile != '#';
+    }
+    
+    let mut successors = Vec::new();
+    let (x, y) = position;
+    if reachable(map[y-1][x]) {
+        successors.push((x, y-1));
+    }
+    if reachable(map[y+1][x]) {
+        successors.push((x, y+1));
+    }
+    if reachable(map[y][x-1]) {
+        successors.push((x-1, y));
+    }
+    if reachable(map[y][x+1]) {
+        successors.push((x+1, y));
+    }
+
+    return successors;
+}
+
+fn unique_path_pattern(keys: &Vec<char>, destination: char) -> String {
+    let mut start: Vec<char> = keys.clone();    
     start.sort();
-    let prefix: String = start.into_iter().collect();
-    return format!("{}/{}", prefix, *keys.last().unwrap());
+    let prefix: String = start.into_iter().unique().filter(|k| *k != destination).collect();
+    return format!("{}/{}", prefix, destination);
 }
 
 impl PossiblePath {
-    fn successors(&self, map: &Vec<Vec<char>>, position: (usize, usize)) -> Vec<((usize, usize), usize)> {
-        
-        fn reachable(tile: char, keys: &Vec<char>) -> bool {
-            return tile == '.' 
-                || tile == '@' 
-                || tile.is_lowercase()
-                || (tile != '#' && tile.is_uppercase() && keys.contains(&tile.to_lowercase().last().unwrap()));
-        }
-        
-        let mut successors = Vec::new();
-        let (x, y) = position;
-        if reachable(map[y-1][x], &(self.keys)) {
-            successors.push(((x, y-1), 1));
-        }
-        if reachable(map[y+1][x], &(self.keys)) {
-            successors.push(((x, y+1), 1));
-        }
-        if reachable(map[y][x-1], &(self.keys)) {
-            successors.push(((x-1, y), 1));
-        }
-        if reachable(map[y][x+1], &(self.keys)) {
-            successors.push(((x+1, y), 1));
-        }
-
-        return successors;
-    }
 
     fn has_all_keys(&self) -> bool {
         return self.remaining.is_empty();
     }
 
-    fn keys_on_path(&self, path: Vec<(usize, usize)>) -> Vec<char> {
-        let mut keys = Vec::new();
-        for position in path.into_iter().skip(1) {
-            for (key, pos) in &self.remaining {
-                if *pos == position {
-                    keys.push(*key);
-                }
+    fn can_open_all_doors(&self, doors: &HashSet<char>, gratis_keys: &HashSet<char>) -> bool {
+        for door in doors {
+            if !self.keys.contains(door) && !gratis_keys.contains(door) {
+                return false;
             }
         }
-        return keys;
+        return true;
     }
 
-    fn possible_continuations(&self, map: &Vec<Vec<char>>) -> Vec<PossiblePath> {
+    fn possible_continuations(&self, graph: &HashMap<char, HashMap<char, (usize, HashSet<char>, HashSet<char>)>>) 
+    -> Vec<PossiblePath> {
         let mut continuations = Vec::new();
-        //println!("looking for paths with these keys: {:?}", self.keys);
+        let starting = self.current_key;
+
         for (key, position) in self.remaining.clone() {
-            let path = dijkstra(&(self.current_position), 
-                |pos| self.successors(map, *pos),
-                |pos| *pos == position);
-            
-            if let Some((working_path, length)) = path {
-                let new_keys = &self.keys_on_path(working_path);
+            let (length, required, gratis_keys) = &graph[&starting][&key];
+
+            if self.can_open_all_doors(required, gratis_keys) {
                 let mut new_continuation = self.clone();
                 new_continuation.path_length += length;
-                for key in new_keys {
-                    new_continuation.remaining.remove(key);
+                
+                for gratis_key in gratis_keys.into_iter() {
+                    if !new_continuation.keys.contains(&gratis_key) {
+                        new_continuation.keys.push(*gratis_key);
+                    }
+                    
+                    new_continuation.remaining.remove(gratis_key);    
                 }
-                new_continuation.keys.extend(new_keys);
+
+                new_continuation.remaining.remove(&key);
                 new_continuation.current_position = position;
+                new_continuation.current_key = key;
 
                 if new_continuation.path_length < 6070 {
                     continuations.push(new_continuation);
                 }
-                
-            }
-            else {
-                //println!("Nothing for: {}, {:?}", key, position);
             }
         }
         
-        continuations.sort_by_key(|c| c.path_length);
-        if continuations.is_empty() {
-            return vec![];
-        }
-        else if continuations.len() == 1 {
-            return vec![continuations[0].clone()];
-        }
-        else {
-            return vec![continuations[0].clone(), continuations[1].clone()];
-        }
+        return continuations;
     }
 }
 
 fn find_keys(map: &Vec<Vec<char>>) -> HashMap<char, (usize, usize)> {
     let mut keys = HashMap::new();
-    for key in ('a' as u8)..('z' as u8) {
+    for key in ('a' as u8)..('z' as u8 + 1) {
         if let Some(position) = location_of(map, key as char) {
             keys.insert(key as char, position);
+        }
+    }
+
+    return keys;
+}
+
+fn find_doors(map: &Vec<Vec<char>>) -> HashMap<char, (usize, usize)> {
+    let mut keys = HashMap::new();
+    for key in ('A' as u8)..('Z' as u8 + 1) {
+        if let Some(position) = location_of(map, key as char) {
+            let door_lower: char = (key as char).to_lowercase().to_string().chars().next().unwrap();
+            keys.insert(door_lower, position);
         }
     }
 
