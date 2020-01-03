bec94d2d9ab61994c7a2bd60c0b5f08c0d419af2
Gasper <gasper@gmx.com>
2020-01-03 13:17:09 +0100

    Solution for part 2 of day 19

--- day19/ninteen.rs
+++ day19/ninteen.rs
@@ -6,6 +6,17 @@ use itertools::Itertools;
 
 mod intcode;
 
+const SHIP_SIZE: i64 = 100;
+
+const NO_PULL: i64 = 0;
+const PULL: i64 = 1;
+
+#[derive(PartialEq, Eq)]
+enum Mode {
+    FindNewEdge,
+    CheckPoints,
+}
+
 fn main() {
     let raw_input = match read("input.txt") {
         Err(_) => panic!("Can't read input.txt!"),
@@ -15,11 +26,16 @@ fn main() {
     let input_string = String::from_utf8_lossy(&raw_input);
     let input_program = get_program(input_string.to_string());
 
-    let mut beam_detection = HashMap::new();
-    let mut all_positions: Vec<(i64, i64)> = (0..100).cartesian_product(0..100).collect();
+    let mut points_to_check: Vec<(i64, i64)> = vec![];
+    let mut current_edge = (5, 8);
+    let mut current_mode: Mode = Mode::FindNewEdge;
+    
+    'main_loop: loop {
+        let (x, y) = current_edge;
 
-    let mut count: i64 = 0;
-    while !all_positions.is_empty() {
+        if current_mode == Mode::FindNewEdge {
+            points_to_check = vec![(x + 1, y + 1)];
+        }
 
         let mut memory = intcode::Memory{
             program: input_program.clone(), 
@@ -28,7 +44,7 @@ fn main() {
         };
 
         let mut continue_from = Some(0);
-        let next = all_positions.pop().unwrap();
+        let next = points_to_check.pop().unwrap();
         let mut input = vec![next.1, next.0];
 
         while continue_from.is_some() {
@@ -42,14 +58,53 @@ fn main() {
             else {
                 assert_eq!(output.len(), 1);
                 let sensor = output.first().unwrap();
-                beam_detection.insert(next, *sensor);
-                count += sensor;
+
+                if current_mode == Mode::FindNewEdge {
+                    if *sensor == PULL {
+                        current_edge = (x + 1, y + 1);
+                    }
+                    else {
+                        current_edge = (x, y + 1);
+                    }
+
+                    println!("New edge: {:?}", current_edge);
+
+                    if x < SHIP_SIZE {
+                        current_mode = Mode::FindNewEdge;
+                    }
+                    else {
+                        current_mode = Mode::CheckPoints;
+
+                        let (new_x, new_y) = current_edge;
+                        points_to_check = generate_points(new_x, new_y);
+                    }
+                }
+                else {
+                    if points_to_check.is_empty() {
+                        println!("The first fit for Santa's ship is X:{}, Y: {}", x-SHIP_SIZE+1, y);
+                        break 'main_loop;
+                    }
+
+                    if *sensor == NO_PULL {
+                        current_mode = Mode::FindNewEdge;
+                    }
+                }
             }
         }
     }
+}
 
-    println!("Detected {} positions with beam", count);
-    paint_screen(beam_detection);
+fn generate_points(x: i64, y: i64) -> Vec<(i64, i64)> {
+    let mut points = vec![];
+    for y_offset in 0..SHIP_SIZE {
+        points.push((x - SHIP_SIZE + 1, y + y_offset));
+    }
+
+    for x_offset in 0..(SHIP_SIZE - 1) {
+        points.push((x - SHIP_SIZE + 2 + x_offset, y));
+    }
+
+    return points;
 }
 
 fn get_program(input: String) -> Vec<i64> {
